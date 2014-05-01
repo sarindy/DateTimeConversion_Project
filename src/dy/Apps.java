c10d37a6740751333c39f5e8147b65be4f29e30b
sarindy <sarindyo@yahoo.com>
2014-05-01 21:38:27 +0700

    Get Current Date and Format
    This Commit is trying to get the current date and format it

--- src/dy/Apps.java
+++ src/dy/Apps.java
@@ -7,25 +7,9 @@ import java.util.Date;
 public class Apps {
 
 	public static void main(String[] args) {
-		SimpleDateFormat formater=new SimpleDateFormat("dd/MM/yyyy");
-		
-		String  dateInString="14/02/1980";
-		try {
-			Date date=formater.parse(dateInString);
-			System.out.println(date);
-			System.out.println(formater.format(date));
-			
-			//Customize the New Date
-			System.out.println("New Customize Date to dd-MMM-yyyy : ");
-			SimpleDateFormat formater1=new SimpleDateFormat("dd-MMM-yyyy");
-			System.out.println(formater1.format(date));
-			
-			
-			
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		SimpleDateFormat formatter=new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a");
+		Date date=new Date();
+		System.out.println(formatter.format(date));
 		
 		
 
