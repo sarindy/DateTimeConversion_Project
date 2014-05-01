83b5efda32a98054ec40f98d929b7287dc664c46
sarindy <sarindyo@yahoo.com>
2014-05-01 21:31:55 +0700

    Convert the String to Date
    This Commit is Convert the String to date and Re-format to another
    format display

--- src/dy/Apps.java
+++ src/dy/Apps.java
@@ -1,9 +1,35 @@
 package dy;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Apps {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SimpleDateFormat formater=new SimpleDateFormat("dd-MM-yyyy");
+		
+		String  dateInString="14-02-1980";
+		try {
+			Date date=formater.parse(dateInString);
+			System.out.println(date);
+			System.out.println(formater.format(date));
+			
+			//Customize the New Date
+			System.out.println("New Customize Date to dd-MMM-yyyy : ");
+			SimpleDateFormat formater1=new SimpleDateFormat("dd-MMM-yyyy");
+			System.out.println(formater1.format(date));
+			
+			
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+		
 
 	}
 
